9c3aa487cbc26c54b65dcfed6a0d116a2674f51c
jamesmartherus <james@martherus.com>
2020-03-26 11:13:00 -0500

    Add function for multi-line axis labels.

---
+++ R/addline_format.R
@@ -0,0 +1,18 @@
+#'Look at this graph
+#'
+#'\code{addline_format} Multi-line axis labels
+#'
+#'  
+#'  
+#'@return NULL
+#'  
+#' @examples
+#' # 
+#' ggplot(counts, aes(x=norm, y=value, fill=type)) +
+#' geom_col() +
+#' scale_x_discrete(labels=addline_format(c("FirstLine SecondLine", "Two-line Label")))
+#' 
+#'@export
+addline_format <- function(x,...){
+  gsub('\\s','\n',x)
+}
